ffee29be2107732d76a7882c9969f1328fa73f14
li10011 <pin80219@gmail.com>
2022-03-26 23:25:35 +0900

    detail design

--- CarRental.Domain/Car/CarDomain.cs
+++ CarRental.Domain/Car/CarDomain.cs
@@ -5,5 +5,9 @@ namespace CarRental.Domain.Car
         public sbyte DoorNum { get; set; }
         
         public sbyte Seat { get; set; }
+
+        public ulong Id { get; set; }
+
+        public decimal? NormalPrice { get; set; }
     }
 }
