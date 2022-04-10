eb9c24879c84c7edc04322f5cfd01ff25d864d03
lorence <aknow90336@gmail.com>
2022-04-10 15:01:24 +0800

    some tweak

--- CarRental.Backend/Startup.cs
+++ CarRental.Backend/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CarRental.Service;
+using CarRental.Service.Impl;
 
 namespace CarRental.Backend
 {
